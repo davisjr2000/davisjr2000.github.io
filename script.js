cd978e043b810dac3061d60039a5022354670d7d
Davis JR <davisjr2000@hotmail.com>
2018-10-17 17:07:42 -0300

    Added bckground pic

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ var person = prompt("Please enter your name", " ");
 
 if (person == null || person == "") {
     txt = "User cancelled the prompt.";
+    alert.txt
 } else {
     txt = "Hello " + person + "! How are you today?";
+    alert.txt
 }
